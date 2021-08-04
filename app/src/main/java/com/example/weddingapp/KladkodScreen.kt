ea91e978be0d5b67847bb6edcbbc2e0de41b511a
Saregard <oskar.saregard@outlook.com>
2021-08-04 16:04:38 +0200

    Added all new screens and configured buttons

---
+++ app/src/main/java/com/example/weddingapp/KladkodScreen.kt
@@ -0,0 +1,11 @@
+package com.example.weddingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KladkodScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kladkod_screen)
+    }
+}
--- app/src/main/java/com/example/weddingapp/MainActivity.kt
+++ app/src/main/java/com/example/weddingapp/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.weddingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weddingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.boendeButton.setOnClickListener {
+            val singUpIntent = Intent(this, BoendeScreen::class.java)
+            startActivity(singUpIntent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+
+        binding.bordsplaceringButton.setOnClickListener {
+            val singUpIntent = Intent(this, BordsplaceringScreen::class.java)
+            startActivity(singUpIntent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+
+        binding.kladkodButton.setOnClickListener {
+            val singUpIntent = Intent(this, KladkodScreen::class.java)
+            startActivity(singUpIntent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+
+        binding.korschemaButton.setOnClickListener {
+            val singUpIntent = Intent(this, KorschemaScreen::class.java)
+            startActivity(singUpIntent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+
+        binding.onskelistaButton.setOnClickListener {
+            val singUpIntent = Intent(this, OnskelistaScreen::class.java)
+            startActivity(singUpIntent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+
+        binding.platsButton.setOnClickListener {
+            val singUpIntent = Intent(this, PlatsScreen::class.java)
+            startActivity(singUpIntent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+
     }
 }
