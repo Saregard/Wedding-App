79309a3f33b61c2932d8e08a45fa4703b76d534c
Saregard <77791326+Saregard@users.noreply.github.com>
2021-08-13 19:27:01 +0200

    Question screen progress

--- app/src/main/java/com/example/weddingapp/QuestionsScreen.kt
+++ app/src/main/java/com/example/weddingapp/QuestionsScreen.kt
@@ -1,16 +1,41 @@
 package com.example.weddingapp
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.weddingapp.databinding.ActivityMainBinding
+import com.example.weddingapp.databinding.ActivityQuestionsScreenBinding
 
 class QuestionsScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuestionsScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_questions_screen)
+
+        binding = ActivityQuestionsScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ninnaEmailIcon.setOnClickListener {
+            copyTextToClipboard()
+        }
+
     }
 
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
     }
+
+    private fun copyTextToClipboard() {
+        val textToCopy = binding.ninnaEmai.text
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText("text", textToCopy)
+        clipboardManager.setPrimaryClip(clipData)
+        Toast.makeText(this, "Text copied to clipboard", Toast.LENGTH_LONG).show()
+    }
+
 }
